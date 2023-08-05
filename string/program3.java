e8e2cdf7a324f8b1643b5007b2fbd52d3f31b626
Shivani9496 <shivaniiiraut9496@gmail.com>
2023-08-05 22:22:00 +0530

    basic string

---
+++ string/program3.java
@@ -0,0 +1,15 @@
+class Demo{
+	public static void main(String [] args){
+		String str = "abcd";
+		int count=0;
+		for(int i=0; i<=str.length()-1;i++){
+			for(int j=i+1;j<=str.length();j++){
+				System.out.println(str.substring(i,j));
+				count++;
+			}
+
+		}
+		System.out.println("Count of substring : " + count);
+	}
+}
+
